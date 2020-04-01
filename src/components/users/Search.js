2c4b7fea968794cbe4a684f3185eff14e3459bf4
Julien Atanassian <julien.atanassian@gmail.com>
2020-04-01 16:54:07 -0400

    search the users when the input is submitted and update the state

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -7,10 +7,16 @@ export class Search extends Component {
 
   onChange = e => this.setState({ [e.target.name]: e.target.value }); // We can just use "text: e.target.value" but using [e.target.name] instead can be useful if we have several inputs (name, email...) so we don't have to create multiple onChange functions
 
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
+  }; // If we use a regular function instead of an arrow function, submitting would result with an error because this is undefined by default. We would have to explicitly bind "this" to this function so in the form add : onSubmit={this.onSubmit.bind(this)}
+
   render() {
     return (
       <div>
-        <form className='form'>
+        <form className='form' onSubmit={this.onSubmit}>
           <input
             type='text'
             name='text'
