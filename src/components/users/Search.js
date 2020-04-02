d5c597ea41b315d763d33319e7525e29a06b1e5a
Julien Atanassian <julien.atanassian@gmail.com>
2020-04-02 09:51:20 -0400

    display the clear button only when users are being shown

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,7 +8,8 @@ export class Search extends Component {
 
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value }); // We can just use "text: e.target.value" but using [e.target.name] instead can be useful if we have several inputs (name, email...) so we don't have to create multiple onChange functions
@@ -20,6 +21,8 @@ export class Search extends Component {
   }; // If we use a regular function instead of an arrow function, submitting would result with an error because this is undefined by default. We would have to explicitly bind "this" to this function so in the form add : onSubmit={this.onSubmit.bind(this)}
 
   render() {
+    const { showClear, clearUsers } = this.props;
+
     return (
       <div>
         <form className='form' onSubmit={this.onSubmit}>
@@ -36,12 +39,11 @@ export class Search extends Component {
             value='Search'
           />
         </form>
-        <button
-          className='btn btn-light btn-block'
-          onClick={this.props.clearUsers}
-        >
-          Clear
-        </button>
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
