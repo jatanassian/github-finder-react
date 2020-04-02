a47f1e73bacf74c21083d20c6ea537c826c9f50c
Julien Atanassian <julien.atanassian@gmail.com>
2020-04-02 10:07:08 -0400

    display a temporary alert when submitting an empty input

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -9,15 +9,20 @@ export class Search extends Component {
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value }); // We can just use "text: e.target.value" but using [e.target.name] instead can be useful if we have several inputs (name, email...) so we don't have to create multiple onChange functions
 
   onSubmit = e => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: '' });
+    if (this.state.text === '') {
+      this.props.setAlert('Please enter something', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
   }; // If we use a regular function instead of an arrow function, submitting would result with an error because this is undefined by default. We would have to explicitly bind "this" to this function so in the form add : onSubmit={this.onSubmit.bind(this)}
 
   render() {
